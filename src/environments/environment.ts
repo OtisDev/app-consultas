1d2bfcfafcb0d2b8c128a3dc26f1745a26e390bc
OtisDev <heyller.otis@gmail.com>
2025-12-11 08:29:09 -0500

    app terminada

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   test:false,
   api_key:'TnbQp52qVustUiLuDkvEvLUzqUiUwlv21IYCsrScYxL2Jr0bfiqfCDmeyrU1',
-  apiUrl: 'https://consultas.muninuevochimbote.gob.pe/api'
+  apiUrl: 'https://backend-consultas.muninuevochimbote.gob.pe/api'
 };
